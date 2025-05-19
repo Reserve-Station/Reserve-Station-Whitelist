4eb19ad5354f8edcc269686ba46e654e85448999
Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
2025-05-19 23:12:27 +0500

    reuse change my email
    BRUH

--- Content.Client/Entry/EntryPoint.cs
+++ Content.Client/Entry/EntryPoint.cs
@@ -114,7 +114,7 @@
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 NazrinNya <137837419+NazrinNya@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 Svarshik <lexasvarshik666@gmail.com>
+// SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 echotry <48294642+echotry-ss14@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 echotry <xippl1@mail.ru>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
