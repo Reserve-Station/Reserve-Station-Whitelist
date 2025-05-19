836fbca2bb96313fdb43a34a5f954f88a5a4ad88
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-05-19 23:43:54 +0000

    chore: Automatically update REUSE headers

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -91,6 +91,7 @@
 // SPDX-FileCopyrightText: 2025 Milon <plmilonpl@gmail.com>
 // SPDX-FileCopyrightText: 2025 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
 // SPDX-FileCopyrightText: 2025 Piras314 <p1r4s@proton.me>
+// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Rouden <149893554+Roudenn@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 SX-7 <92227810+SX-7@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
