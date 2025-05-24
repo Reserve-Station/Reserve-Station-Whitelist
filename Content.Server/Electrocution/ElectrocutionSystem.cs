2003d394002ca9b6b57604e5040e976f14937c26
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-05-24 23:17:00 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Electrocution/ElectrocutionSystem.cs
+++ Content.Server/Electrocution/ElectrocutionSystem.cs
@@ -45,6 +45,7 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 J <billsmith116@gmail.com>
+// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 deltanedas <39013340+deltanedas@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 nazrin <tikufaev@outlook.com>
