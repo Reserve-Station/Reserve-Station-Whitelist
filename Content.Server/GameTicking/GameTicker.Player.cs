a630d1516a56f112c6b1b8bd9a5950c60ebc7a04
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-05-25 12:18:04 +0000

    chore: Automatically update REUSE headers

--- Content.Server/GameTicking/GameTicker.Player.cs
+++ Content.Server/GameTicking/GameTicker.Player.cs
@@ -26,6 +26,7 @@
 // SPDX-FileCopyrightText: 2024 nikthechampiongr <32041239+nikthechampiongr@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 J <billsmith116@gmail.com>
+// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 nazrin <tikufaev@outlook.com>
