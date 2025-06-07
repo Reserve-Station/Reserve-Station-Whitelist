1ab171f587f9ac09e92c3faa56a3685128565648
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-06-07 20:49:31 +0000

    chore: Automatically update REUSE headers

--- Content.Server/StationEvents/Events/PowerGridCheckRule.cs
+++ Content.Server/StationEvents/Events/PowerGridCheckRule.cs
@@ -9,6 +9,7 @@
 // SPDX-FileCopyrightText: 2024 deltanedas <39013340+deltanedas@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 deltanedas <@deltanedas:kde.org>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Winkarst <74284083+Winkarst-cpu@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 echotry <xippl1@mail.ru>
