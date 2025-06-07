4b9abcf36d6025e864e44f8a763c301b82225520
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-06-07 22:28:14 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Emoting/EmotingComponent.cs
+++ Content.Shared/Emoting/EmotingComponent.cs
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2022 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2023 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 nazrin <tikufaev@outlook.com>
 //
 // SPDX-License-Identifier: MIT
 
