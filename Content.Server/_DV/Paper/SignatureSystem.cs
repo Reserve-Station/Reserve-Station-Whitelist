57334fcc7ea9cae3ab2e582a54e5e0a36a364e99
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-06-12 20:34:25 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_DV/Paper/SignatureSystem.cs
+++ Content.Server/_DV/Paper/SignatureSystem.cs
@@ -1,5 +1,10 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 AwareFoxy <135021509+AwareFoxy@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 NazrinNya <137837419+NazrinNya@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Solstice <solsticeofthewinter@gmail.com>
+// SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 nazrin <tikufaev@outlook.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
